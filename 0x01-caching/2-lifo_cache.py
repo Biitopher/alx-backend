429f618292a13d5778fd2c3992fc847f9db3e7e1
Biitopher <biitopher@gmail.com>
2024-01-30 22:33:15 +0000

    Task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -4,14 +4,14 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """Defines class  LIFO Cache"""
+    """Defines class  LIFO Cache that inherits from BaseCaching"""
     def __init__(self):
-        """Initiate"""
+        """Initialize the LIFO cache"""
         super().__init__()
         self.discarded_key = ''
 
     def put(self, key, item):
-        """Put"""
+        """Checks if the number of items exceed"""
         if key is not None and item is not None:
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
@@ -20,7 +20,7 @@ class LIFOCache(BaseCaching):
             self.discarded_key = key
 
     def get(self, key):
-        """Get"""
+        """Returns vallue associated with key"""
         if key is None or self.cache_data.get(key) is None:
             return None
         if key in self.cache_data:

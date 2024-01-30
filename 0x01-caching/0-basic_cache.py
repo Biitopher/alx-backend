6f4a856be9c879a2562d609d98f35658df3a437d
Biitopher <biitopher@gmail.com>
2024-01-30 22:19:25 +0000

    Task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,7 +10,6 @@ class BasicCache(BaseCaching):
         if key and item:
             self.cache_data[key] = item
 
-
     def get(self, key):
         """Get cache to return value"""
         if key is None or self.cache_data.get(key) is None:

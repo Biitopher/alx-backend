8c14d4918330d3a85302d22258e17754a8c876cf
Biitopher <biitopher@gmail.com>
2024-02-06 16:24:31 +0000

    Task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Parametrize templates"""
+"""implement a way to force a particular locale"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
@@ -13,20 +13,24 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
 app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
-    """defines the locale"""
+    """Check if the 'locale' parameter is present in the request"""
+    requested_locale = request.args.get('locale')
+
+    if requested_locale and requested_locale in app.config['LANGUAGES']:
+        return requested_locale
+
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
-    """define the index"""
-    return render_template('3-index.html')
+    """defines the index"""
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
